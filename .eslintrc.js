ebf73e1822932e4afcbddf62bb828eb6c2699629
EliteCheng <chengtang9528@163.com>
2019-10-12 21:25:25 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,12 +11,12 @@ module.exports = {
     "rules": {
         "jsx-a11y/alt-text": "off",
         "jsx-a11y/anchor-is-valid": "off",
-        // "eqeqeq": "off",
-        // "no-useless-escape": "off",
-        // "default-case": "off",
-        // "no-unused-vars": "off",
-        // "no-useless-constructor": "off",
-        // "react/no-direct-mutation-state": "off",
-        // "no-loop-func": "off",
+        "eqeqeq": "off",
+        "no-useless-escape": "off",
+        "default-case": "off",
+        "no-unused-vars": "off",
+        "no-useless-constructor": "off",
+        "react/no-direct-mutation-state": "off",
+        "no-loop-func": "off",
     }
-}
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+# 构建React项目
+
+```shell
+1.安装node
+2.全局安装yarn，npm i yarn -g
+3.全局安装create-react-app，npm i create-react-app -g
+4.查看版本 create-react-app --version
+node -v
+npm -v
+5.创建react项目，create-react-app xxx【项目只允许小写】
+```
+
+## Yarn
+
+[yarn官方中文文档](https://yarn.bootcss.com/docs/getting-started)
+
